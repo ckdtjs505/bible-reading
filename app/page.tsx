c646d6f8a86a0380ab7196e87ba5cf4813d7cc6f
소니아_오창선 <ckdtjs505@sooplive.com>
2025-01-21 16:56:01 +0900

    ✨ feat(calender): add react-calender

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Calender from "@/client/components/ClientCalender";
+
 export default function Home() {
   return (
     <>
       <div id="title" className="text-center text-2xl ">
         함 온 성
       </div>
-
+      <Calender></Calender>
       <br />
     </>
   );
