286a70d3c931ee5d94ef1fe290b6cc66be8548fd
소니아_오창선 <ckdtjs505@sooplive.com>
2025-01-23 13:23:17 +0900

    ✨ feat(api): create api

---
+++ components/Calendar.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import "./Calendar.css";
+import { Calendar as RCalender } from "react-calendar";
+import React, { useEffect, useState } from "react";
+import { getBiblePlan, getDailyVerse } from "@/services/api/biblePlan";
+import { BiblePlan } from "@/type/biblePlan";
+
+const Calender: React.FC = () => {
+  const [planInfo, setPlanInfo] = useState<BiblePlan>([]);
+
+  useEffect(() => {
+    getBiblePlan().then(({ data }) => {
+      setPlanInfo(data as BiblePlan);
+    });
+  }, []);
+
+  const tileContent = ({ date }) => {
+    const currentDate = new Date(date);
+    const currentDateYYYYMMDD = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
+    const planInd = planInfo?.findIndex(
+      (plan) => plan.date === currentDateYYYYMMDD,
+    );
+
+    if (planInd >= 0) {
+      return (
+        <div>
+          {planInfo[planInd].book}
+          <br />
+          {planInfo[planInd].start}-{planInfo[planInd].end}장
+        </div>
+      );
+    }
+  };
+
+  const handleClickDay = (date) => {
+    const currentDate = new Date(date);
+    const currentDateYYYYMMDD = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
+    const planInd = planInfo?.findIndex(
+      (plan) => plan.date === currentDateYYYYMMDD,
+    );
+
+    if (planInd >= 0) {
+      getDailyVerse({
+        book: planInfo[planInd].book,
+        start: planInfo[planInd].start,
+        end: planInfo[planInd].end,
+      }).then((data) => {
+        console.log(data);
+      });
+    }
+  };
+
+  return (
+    <RCalender
+      formatDay={(_, date) => {
+        return date
+          .toLocaleString("ko-KR", { day: "2-digit" })
+          .replace("일", "");
+      }}
+      calendarType="gregory"
+      showWeekNumbers={false}
+      next2Label={""}
+      prev2Label={""}
+      prevLabel={"<"}
+      nextLabel={">"}
+      showNeighboringMonth={false}
+      tileContent={tileContent}
+      onClickDay={handleClickDay}
+    ></RCalender>
+  );
+};
+
+export default Calender;
--- pages/api/biblePlan.ts
+++ pages/api/biblePlan.ts
@@ -1,19 +1,23 @@
 import { BiblePlan } from "@/type/biblePlan";
+import { NextApiRequest, NextApiResponse } from "next";
 
 const GOOGLE_DOMAIN = "https://script.google.com";
 const GOOGLE_KEY = `AKfycbx59b6woS9-hkh8jkk93zrBUOSwbiI6JvBQT0-wdP-zxD_dNFrnL_t5WNvuulvzNtOq`;
 
-export const getBiblePlan = (): Promise<BiblePlan> => {
+export const getBiblePlan = async (): Promise<BiblePlan> => {
   const queryParam = new URLSearchParams({
     type: "admin",
     userName: "오창선",
   });
 
-  return fetch(`${GOOGLE_DOMAIN}/macros/s/${GOOGLE_KEY}/exec?${queryParam}`, {
-    headers: {
-      "content-Type": "text/plain;charset=utf-8",
+  return await fetch(
+    `${GOOGLE_DOMAIN}/macros/s/${GOOGLE_KEY}/exec?${queryParam}`,
+    {
+      headers: {
+        "content-Type": "text/plain;charset=utf-8",
+      },
     },
-  })
+  )
     .then((response) => {
       const res = response.json();
       return res;
@@ -21,6 +25,15 @@ export const getBiblePlan = (): Promise<BiblePlan> => {
     .catch((e) => {
       console.log(e);
     });
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const BiblePlans = await getBiblePlan();
+    res.status(200).json(BiblePlans);
+  } catch (error) {
+    res.status(500).json({ message: "error fetching" });
+  }
 };
 
 type DailyVeserParams = {
