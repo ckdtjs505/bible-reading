8f9a6a7edf6a442745119c748c5de4518ddf8220
소니아_오창선 <ckdtjs505@sooplive.com>
2025-01-24 21:29:19 +0900

    ✨ feat(message): add marked message

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -2,7 +2,12 @@
 
 import "./Calendar.css";
 import { Calendar as RCalendar } from "react-calendar";
-import React, { useEffect, useState } from "react";
+import React, {
+  MouseEvent,
+  MouseEventHandler,
+  useEffect,
+  useState,
+} from "react";
 import { Plan, Verse } from "@/type/biblePlan";
 import { getBiblePlan } from "@/pages/api/biblePlan";
 import { getDailyVerse } from "@/pages/api/bible";
@@ -13,8 +18,8 @@ const Calendar: React.FC = () => {
   const [verse, setVerse] = useState<{ book?: string; data?: Verse[] }>({});
   const [img, setImg] = useState<string>("");
   const [videoId, setVideoId] = useState<string>("");
-  const { fontLevel  } = useFontLevel();
-
+  const { fontLevel } = useFontLevel();
+  const [selectList, setSelectList] = useState<string[]>([]);
   useEffect(() => {
     const value = getBiblePlan();
     setPlanInfo(value);
@@ -64,17 +69,23 @@ const Calendar: React.FC = () => {
       setImg(planInfo[planInd].img);
       setVerse({ book: planInfo[planInd].book, data });
     } else {
-      setVideoId('');
-      setImg('');
-      setVerse({ book: '', data : [] });
-
+      setVideoId("");
+      setImg("");
+      setVerse({ book: "", data: [] });
     }
   };
 
-  const handleClickMessage = () => { 
+  const handleClickMessage = (event: React.MouseEventHandler<HTMLElement>) => {
+    const target = event?.target as HTMLElement;
 
-    
-  }
+    if (target.classList.contains("active")) {
+      target.classList.remove("active");
+    } else {
+      target.classList.add("active");
+      setSelectList([...selectList, target.innerText]);
+    }
+  };
+
   return (
     <div className="flex justify-center flex-col">
       <RCalendar
@@ -98,25 +109,68 @@ const Calendar: React.FC = () => {
         <iframe
           width={"100%"}
           height="315"
-          className="p-2 "
+          className="p-4 "
           src={`https://www.youtube.com/embed/${videoId}`}
           title="함온성"
         ></iframe>
       )}
-      {img && <img src={img} alt="img" className="p-2 w-full"></img>}
 
-      <div className="m-2 text-2xl">
+      {img && <img src={img} alt="img" className="p-4 w-full"></img>}
+
+      <div className="p-4 text-2xl">
         <div>{verse.book}</div>
         {verse.data?.map(({ chapter, verse, message }, index) => {
           return (
             <div key={index}>
-              <div className={`${fontLevel}`} onClick={ handleClickMessage}>
+              <div className={`${fontLevel}`} onClick={handleClickMessage}>
                 {chapter}:{verse} {message}
               </div>
               <br />
             </div>
           );
         })}
+      </div>
+
+      <div>
+        <div id="userContent" className="p-4">
+          <div id="submitdata">
+            <div id="prayForUser">
+              💝앞사람을 위한 기도 :
+              <textarea
+                id="prayForUserText"
+                className="w-full h-20 border border-black"
+              ></textarea>
+            </div>
+            <div>
+              🌼 이름 : <span id="name">오창선</span>
+            </div>
+            📖 오늘 내게 주신 말씀 : <br />
+            <div id="myMessage">
+              {selectList.map((value, index) => {
+                return (
+                  <div key={index}>
+                    {value} <br />
+                  </div>
+                );
+              })}
+            </div>
+            <div id="prayBox">
+              🙏 한줄 기도 :
+              <textarea
+                id="pray"
+                className="w-full h-20 border border-black"
+              ></textarea>
+            </div>
+            제 <span id="day"> 17 </span> 일차 완료했습니다. <br />
+          </div>
+
+          <button id="saveButton" type="button">
+            복사하기
+          </button>
+          <button id="changeName" type="button">
+            이름바꾸기
+          </button>
+        </div>
       </div>
     </div>
   );
