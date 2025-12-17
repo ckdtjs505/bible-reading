2fa197cb3b794de44567a2bd496d676c01b535fb
ckdtjs505 <ckdtjs505@naver.com>
2025-12-17 17:18:05 +0900

    fix :vervel build error

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,4 @@
-
 import mongoose from 'mongoose';
-
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-    throw new Error(
-        'Please define the MONGODB_URI environment variable inside .env.local'
-    );
-}
 
 
 /**
@@ -33,6 +24,14 @@ if (!cached) {
 }
 
 async function dbConnect() {
+    const MONGODB_URI = process.env.MONGODB_URI;
+
+    if (!MONGODB_URI) {
+        throw new Error(
+            'Please define the MONGODB_URI environment variable inside .env.local'
+        );
+    }
+
     if (cached.conn) {
         return cached.conn;
     }
@@ -42,7 +41,7 @@ async function dbConnect() {
             bufferCommands: false,
         };
 
-        cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
             return mongoose;
         });
     }
