d5c47c84b612a4b7f32a402e7934517bc252e47d
ckdtjs505 <ckdtjs505@naver.com>
2026-01-06 06:09:02 +0900

    fix : 함온성 진행사항 구현

--- pages/api/userInfo.ts
+++ pages/api/userInfo.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 const GOOGLE_API = `https://script.google.com`;
-const GOOGLE_KEY = `AKfycbyY1CrlExY2T7tJhwYTes3TGlZFC6N7YuC0VCMexaIMLLiMDhWBPssbNrUeThtP4ZOo`;
+const GOOGLE_KEY = `AKfycbz7h7vGZO3wuvURMKqwqBJHSMbQxJ_8t4ZTC0icDb1BOEF-Gi1SqyCEBMLXXqeGbogF`;
 
 export const getUserProgressInfo = async (userName: string) => {
   const queryParams = new URLSearchParams({
